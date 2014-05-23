f0f9e15849f8cb1601ed12ac4f86e6304a68508b
Christian Rebelsky <christian.rebelsky@gmail.com>
2014-05-23 23:09:36 +0200

    Swap Bootstrap with Foundation ADD: Foundation 5.2.2 DEL: Bootstrap MOD: package.json (higher node version for further development), bower.json (new dependencies like mentioned)

---
+++ app/scripts/main.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: Christian Rebelsky
+ * @Date:   2014-05-21 06:58:29
+ * @Last Modified by:   Christian Rebelsky
+ * @Last Modified time: 2014-05-23 23:02:58
+ */
+
+/*jslint browser: true, devel: true, nomen: true*/
+/*global $:false*/
+
+'use strict';
+
+$(document).ready(function () {
+    $(document).foundation();
+});
