7160ecb5021dc1451d4f04bfe8b2cc91d2630481
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-06-03 23:14:20 +0200

    next try

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,13 +8,13 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     # setup ssh agent, git config and remote
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
-    git remote add deploy "travis@webhost.christianrebelsky.com:/var/www/christianrebelsky"
+    git remote add deploy "travis@christianrebelsky.com:/var/www/christianrebelsky"
     git config user.name "Travis CI"
     git config user.email "travis@christianrebelsky.com"
 
     # commit compressed files and push it to remote
     rm -f .gitignore
-    cp .travis/deployignore .gitignore
+    cp .gitignore
     git add .
     git status # debug
     git commit -m "Deploy compressed files"
@@ -22,6 +22,19 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
 
 else
 
-    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+    git remote add deploy "travis@christianrebelsky.com:/var/www/christianrebelsky/development"
+    git config user.name "Travis CI"
+    git config user.email "travis@christianrebelsky.com"
+
+    # commit compressed files and push it to remote
+    rm -f .gitignore
+    cp .gitignore
+    git add .
+    git status # debug
+    git commit -m "Deploy compressed files"
+    git push -f deploy HEAD:master
 
 fi
