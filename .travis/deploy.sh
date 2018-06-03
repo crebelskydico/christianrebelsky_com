b176deab629477dd99648c66e8fd70b7e4be64b2
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-06-03 23:39:59 +0200

    simplify

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -9,12 +9,5 @@ ssh-add ~/.ssh/travis_rsa
 git remote add deploy "travis@christianrebelsky.com:/var/www/christianrebelsky"
 git config user.name "Travis CI"
 git config user.email "travis@christianrebelsky.com"
-
-# commit compressed files and push it to remote
-rm -f .gitignore
-cp .gitignore
-git add .
-git status # debug
-git commit -m "Deploy compressed files"
-git push -f deploy HEAD:master
+git push -f deploy
 
