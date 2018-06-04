80291d21cd156bec6a8f8b3a6ff213351fefdcf8
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-06-04 07:45:51 +0200

    change deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,8 @@ set -xe
 # setup ssh agent, git config and remote
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis_rsa
-git push origin master:refs/heads/master
 git remote add deploy "travis@christianrebelsky.com:/var/www/christianrebelsky"
+git push deploy master:refs/heads/master
 git config user.name "Travis CI"
 git config user.email "travis@christianrebelsky.com"
 git push -f deploy HEAD:master
