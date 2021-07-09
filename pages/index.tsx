543b79993b91e11e70c58eee55d208c6f7310a22
Christian Rebelsky <christian.rebelsky@gmail.com>
2021-07-09 13:38:18 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
-import Head from 'next/head'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+//NOTE: grid, typo, menu
 
 export default function Home(): JSX.Element {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    <div className="grid grid-rows-4 h-screen">
+      <Header />
+      <main className="grid grid-cols-1 lg:grid-cols-3 lg:gap-4 row-start-3">
+        <div className="lg:col-start-2 lg:col-end-3 self-start justify-self-center">
+          <h1 className="text-primary font-sans font-normal text-3xl lg:text-5xl">
+            Coming Soon
+          </h1>
+        </div>
       </main>
-
-      <footer>Footer</footer>
+      <Footer />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#4A5865',
+      },
+      gridTemplateRows: {
+        content: 'auto 1fr auto',
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
