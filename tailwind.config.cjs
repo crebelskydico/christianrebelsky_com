7bf5285828659beb2fd8861ff09510a2094d1d8c
Christian Rebelsky <christian.rebelsky@gmail.com>
2022-07-18 11:21:45 +0200

    add ga, update tailwind config for v3

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-  	purge: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
-	darkMode: false, // or 'media' or 'class'
+  	content: ['./public/**/*.html', './src/**/*.{astro,js,jsx,ts,tsx,vue}'],
 	theme: {
 		extend: {
-		colors: {
-			primary: '#4A5865',
+			colors: {
+				primary: '#4A5865',
+			},
+			gridTemplateRows: {
+				content: 'auto 1fr auto',
+			},
 		},
-		gridTemplateRows: {
-			content: 'auto 1fr auto',
-		},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+	}
 }
