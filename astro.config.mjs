e3144378b192235b5fbbcda1fdb8c103da9c7769
Christian Rebelsky <christian.rebelsky@gmail.com>
2025-11-23 07:05:04 +0100

    remove vercel plugin for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config'
-import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  adapter: vercel(),
-
   vite: {
     plugins: [tailwindcss()],
   },
